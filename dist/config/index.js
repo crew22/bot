4d9e81271c29464243af66e89322076d1eadca36
Ata Ege Asiltürk <ataegeasilturk@gmail.com>
2021-05-21 00:37:22 +0300

    update: gitignore

--- dist/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    defaultPrefix: '!',
-    defaultLanguage: 'tr',
-};
-exports.default = config;
-//# sourceMappingURL=index.js.map
--- dist/events/Client Events/ready.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.name = exports.run = void 0;
-const run = async (client) => {
-    client.logger.success(`${client.user.tag} is now online!`);
-};
-exports.run = run;
-exports.name = 'ready';
-//# sourceMappingURL=ready.js.map
